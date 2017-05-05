303f655a5c7ac3f592b99bc5c4fe59198880939a
levi <levi@exabeam.com>
2017-05-05 18:21:57 +0800

    using queue

--- easy_stat.py
+++ easy_stat.py
@@ -6,7 +6,8 @@ from time import sleep
 import json
 from pymongo import MongoClient
 import logging
-logformat=logging.Formatter("%(message)s %(asctime)s %(filename)s[line:%(lineno)d] \
+from multiprocessing import Process, Queue
+logformat=logging.Formatter("%(message)s %(asctime)s \
                      %(levelname)s ")
 loginfo=logging.getLogger('info')
 loginfo.setLevel(logging.INFO)
@@ -20,57 +21,70 @@ logerror.addHandler(ferror)
 
 BASE_URL="http://300report.jumpw.com/"
 STARTID=72008655
-TIME_SLEEP=2
+TIME_SLEEP=0.1
 MONGODB_HOST=27017
 DB_NAME='SanBaiHeros'
 COLLECTION_NAME='match_info'
 
 class MatchStat():
     client=MongoClient('localhost',MONGODB_HOST)
-    db=client[DB_NAME]
-    coll=db[COLLECTION_NAME]
-    def collect_match(self,matchid):
-        no = int(matchid - STARTID + 1)
-        try:
-            html=requests.get(BASE_URL+'api/getmatch?id=%d'%matchid)
-            if html.status_code==200:
-                user=json.loads(html.text)
-                if user['Result'] and user['Result']=='OK': #and self.coll.find({'matchid':matchid}).count()==0:
-                    user['matchid']=matchid
-                    user['no']=no
-                    #sleep(TIME_SLEEP)
-                    #print user
-                    return user
-        except Exception,e:
-            loginfo.warn("unable to get match:%d,id:%d"%(matchid,matchid-STARTID)+str(e))
-
-    def stat_match(self,start,end):
-        list=[]
-        j=0
+    coll=client[DB_NAME][COLLECTION_NAME]
+    uq=Queue(maxsize=1000)
+    dq=Queue(maxsize=1000)
+    mq=Queue(maxsize=10)
+    def putID(self,uq,start,end):
         for i in range(start,end):
-            if j<1000:
+            while not uq.full():
+                uq.put(i)
+
+    def collectMatch(self,uq,dq):
+        while True:
+            while not uq.empty():
+                matchid = uq.get_nowait()
+                no = int(matchid - STARTID + 1)
                 try:
-                    user=self.collect_match(i+STARTID)
-                    if user:
-                        list.append(user)
-                        j += 1
-                    else:
-                        pass
+                    html=requests.get(BASE_URL+'api/getmatch?id=%d'%matchid)
+                    if html.status_code==200:
+                        user=json.loads(html.text)
+                        if user['Result'] and user['Result']=='OK' and \
+                                        self.coll.find({'no':no}).count()==0:
+                            user['matchid']=matchid
+                            user['no']=no
+                            sleep(TIME_SLEEP)
+                            #print user
+                            dq.put(user)
                 except Exception:
-                    pass
-            else:
-                try:
-                    self.coll.insert_many(list)
-                    loginfo.info("Data saved,id:%d"%(i/1000))
-                except Exception,e:
-                    logerror.error("Write data failed,id:%d"%(i/1000)+str(e))
-                j=0
-                list=[]
-        try:
-            self.coll.insert_many(list)
-            loginfo.info("Data saved,id:%d" % (i / 1000))
-        except Exception, e:
-            logerror.error("Write data failed,id:%d" % (i/1000)+str(e))
+                    uq.put(matchid)
+
+    def putMatch(self,dq,mq):
+        list=[]
+        while True:
+            for i in range(1000):
+                list.append(dq.get(True))
+            mq.put(list)
+            list=[]
+
+    def writeData(self,mq):
+        while True:
+            list=mq.get()
+            i=int(list[0]['no'])
+            try:
+                self.coll.insert_many(list)
+                loginfo.info("Data saved,id:%d"%(i/1000))
+            except Exception,e:
+                logerror.error("Write data failed,id:%d"%(i/1000)+str(e))
+                mq.put(list)
+
+    def run(self,start,end):
+        p1=Process(target=self.putID,args=(self.uq,start,end,))
+        p2=Process(target=self.collectMatch,args=(self.uq,self.dq,))
+        p3=Process(target=self.putMatch,args=(self.dq,self.mq,))
+        p4=Process(target=self.writeData,args=(self.mq,))
+        pList=[p1,p2,p3,p4]
+        for p in pList:
+            p.start()
+            p.join()
+
 
 if __name__=='__main__':
     m=MatchStat()
@@ -79,4 +93,4 @@ if __name__=='__main__':
         TIME_SLEEP=float(sys.argv[3])
     except:
         pass
-    m.stat_match(int(sys.argv[1]),int(sys.argv[2]))
+    m.run(int(sys.argv[1]),int(sys.argv[2]))
--- get_stats.py
+++ get_stats.py
@@ -1,19 +1,25 @@
+#coding:utf-8
 import pymongo
 client=pymongo.MongoClient('localhost',27017)
 coll=client['SanBaiHeros']['match_info']
 win_stat={}
+STARTNO=900000
+ENDNO=1000001
 with open('Heros.txt') as f:
     hero_list=f.readlines()
-for h in hero_list:
-    h=h.strip()
-    print "stating hero %s"%h
-    wins=coll.find({"Match.WinSide.Hero.Name":h,"Match.MatchType":1,\
-                    "$where":"this.no > 900000"}).count()
-    lose=coll.find({"Match.LoseSide.Hero.Name":h,"Match.MatchType":1,\
-                    "$where":"this.no > 900000"}).count()
-    both=coll.find({'Match.WinSide.Hero.Name':h,'Match.LoseSide.Hero.Name':h,\
-                            'Match.MatchType':1,"$where":"this.no > 900000"}).count()
-    win_stat[h]=str(wins)+':'+str(lose)+':'+str(both)
+    #print hero_list[0].decode('utf8')
+
+def get_stats(hero_list):
+    for h in hero_list:
+        h=h.strip()
+        print "stating hero %s"%h.decode('utf8')
+        wins=coll.find({"$where":"this.no > %d && this.no < %d"%(STARTNO,ENDNO),\
+                        "Match.WinSide.Hero.Name":h,"Match.MatchType":1}).count()
+        lose=coll.find({"$where":"this.no > %d && this.no < %d"%(STARTNO,ENDNO),\
+                        "Match.LoseSide.Hero.Name":h,"Match.MatchType":1,}).count()
+        both=coll.find({"$where":"this.no > %d && this.no < %d"%(STARTNO,ENDNO),'\
+        Match.WinSide.Hero.Name':h,'Match.LoseSide.Hero.Name':h}).count()
+        win_stat[h]=str(wins)+':'+str(lose)+':'+str(both)
 
 #print win_stat
 def writeResult():
@@ -22,11 +28,12 @@ def writeResult():
             f.write(w+':'+win_stat[w]+'\n')
 
 def statWins():
-    with open('winResult.txt', 'w') as w:
+    with open('winResult_%dw-%dw.txt'%(int(STARTNO/10000),int(ENDNO/10000)), 'w') as w:
         for k,v in win_stat.items():
             list = v.strip().split(':')
             win_p = float(
                 (float(list[1]) - float(list[3])) / (float(list[1]) + float(list[2]) - 2 * float(list[3])))
             w.write(k + ':' + '%0.3f' % win_p + '\n')
 
+get_stats(hero_list)
 statWins()
